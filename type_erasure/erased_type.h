75a5771a1cc0a63f547cd50fdddabe362e4c2130
Garetonchick <dsbonich@edu.hse.ru>
2023-04-08 16:11:55 +0300

    Expirement with type erasure

---
+++ type_erasure/erased_type.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <memory>
+
+namespace ani {
+
+template <class Interface, template <class> class Caller, template <class, class> class Implementor>
+class ErasedType : public Caller<std::shared_ptr<const Interface>> {
+    using CallerT = Caller<std::shared_ptr<const Interface>>;
+
+public:
+    template <class T>
+    ErasedType(T val) : CallerT(std::make_shared<const Implementor<T, Interface>>(val)) {
+    }
+};
+
+}  // namespace ani
